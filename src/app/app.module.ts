794ec6ddb9545789ef96c80ef5f69c83b0243900
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-03-23 18:41:51 -0300

    feat: inclusao de import no AppRoutingModule no app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppRoutingModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
